14466ba1fd611d8a72b576031e5666361262f84c
Admiral-White <mikeinyang975@gmail.com>
2021-05-17 15:51:21 +0100

    commit after some adjustment, application running but json server not running.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from "./store";
+
+// This is used for top level configurations on the project
+// some dependencies that will be used in the project must be referenced here.
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "font-awesome/css/font-awesome.min.css"
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')
