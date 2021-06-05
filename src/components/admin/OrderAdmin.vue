e513b1859c4b94fc11e78265425e494185ba1e2c
Admiral-White <mikeinyang975@gmail.com>
2021-06-06 00:28:22 +0100

    more admin functionality

---
+++ src/components/admin/OrderAdmin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-danger text-white text-center h4 p-2">
+    Order Admin
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrderAdmin"
+}
+</script>
+
+<style scoped>
+
+</style>
