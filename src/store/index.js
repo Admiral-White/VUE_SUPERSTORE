56388a7da202bcddd8ba95f623ba284d0be22ae5
Admiral-White <mikeinyang975@gmail.com>
2021-06-07 03:15:31 +0100

    ...

--- src/store/index.js
+++ src/store/index.js
@@ -244,7 +244,10 @@ export default new Vuex.Store({
             return state.pages[state.currentPage];
         },
         pageCount: (state) => state.serverPageCount,
-        categories: state => ["All", ...state.categoriesData]
+        categories: state => ["All", ...state.categoriesData],
+        productById:(state) => (id) => {
+            return state.pages[state.currentPage].find(p => p.id === id);
+        }
     },
     mutations: {
         _setCurrentPage(state, page) {
@@ -281,6 +284,14 @@ export default new Vuex.Store({
             state.searchTerm = term;
             state.currentPage = 1;
         },
+        _addProduct(state, product) {
+            state.pages[state.currentPage].unshift(product);
+        },
+        _updateProduct(state, product) {
+            let page = state.pages[state.currentPage];
+            let index = page.findIndex(p => p.id === product.id);
+            Vue.set(page, index, product);
+        }
     },
 
     actions: {
@@ -288,6 +299,26 @@ export default new Vuex.Store({
             await context.dispatch("getPage", 2);
             context.commit("setCategories", (await Axios.get(categoriesUrl)).data);
         },
+
+        async addProduct(context, product) {
+            let data = (await context.getters.authenticatedAxios.post(productsUrl,
+                product)).data;
+            product.id = data.id;
+            this.commit("_addProduct", product);
+        },
+        async removeProduct(context, product) {
+            await context.getters.authenticatedAxios
+                .delete(`${productsUrl}/${product.id}`);
+            context.commit("clearPages");
+            context.dispatch("getPage", 1);
+        },
+
+        async updateProduct(context, product) {
+            await context.getters.authenticatedAxios
+                .put(`${productsUrl}/${product.id}`, product);
+            this.commit("_updateProduct", product);
+        },
+
         async getPage(context, getPageCount = 1) {
             let url = `${productsUrl}?_page=${context.state.currentPage}`
                 + `&_limit=${context.state.pageSize * getPageCount}`;
